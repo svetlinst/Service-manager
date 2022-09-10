3d344fa79df19b8d81b1a49e7d9dd41dfba69ba3
svetlinst <svetlinst@gmail.com>
2022-09-11 00:16:04 +0300

    Add basic Customers List and Update views

--- service_manager/main/urls.py
+++ service_manager/main/urls.py
@@ -2,8 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from service_manager.main.views import get_index
+from service_manager.main.views import get_index, CustomersListView, CustomerUpdateView
 
 urlpatterns = [
-                  path('', get_index, name='index')
+                  path('', get_index, name='index'),
+                  path('customers/', CustomersListView.as_view(), name='customers_list'),
+    path('customers/<int:pk>/', CustomerUpdateView.as_view(), name='customer_details'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- service_manager/main/views.py
+++ service_manager/main/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import django.views.generic as views
+
+from service_manager.main.models import Customer
 
 
 def get_index(request):
     return render(request, 'index.html')
+
+
+class CustomersListView(views.ListView):
+    model = Customer
+    template_name = 'customers.html'
+    ordering = ('name',)
+
+
+class CustomerUpdateView(views.UpdateView):
+    model = Customer
+    fields = '__all__'
+    template_name = 'customer_details.html'
