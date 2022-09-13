da5db23e586f3670ebbd13ee5ca1bd9608af3811
svetlinst <svetlinst@gmail.com>
2022-09-13 11:47:41 +0300

    Refactor the template folder into entity categories

--- service_manager/main/views.py
+++ service_manager/main/views.py
@@ -14,77 +14,77 @@ def get_index(request):
 
 class CustomersListView(views.ListView):
     model = Customer
-    template_name = 'customers.html'
+    template_name = 'customer/customers.html'
     ordering = ('name',)
 
 
 class EditCustomerView(views.UpdateView):
     model = Customer
     form_class = EditCustomerForm
-    template_name = 'customer_edit.html'
+    template_name = 'customer/customer_edit.html'
     success_url = reverse_lazy('customers_list')
 
 
 class CreateCustomerView(views.CreateView):
     model = Customer
     form_class = CreateCustomerForm
-    template_name = 'customer_create.html'
+    template_name = 'customer/customer_create.html'
     success_url = reverse_lazy('customers_list')
 
 
 class DeleteCustomerView(views.DeleteView):
     model = Customer
-    template_name = 'customer_delete.html'
+    template_name = 'customer/customer_delete.html'
     success_url = reverse_lazy('customers_list')
 
 
 class AssetsListView(views.ListView):
     model = Asset
-    template_name = 'assets.html'
+    template_name = 'asset/assets.html'
     ordering = ('model_name', 'model_number')
 
 
 class CreateAssetView(views.CreateView):
     model = Asset
     form_class = CreateAssetForm
-    template_name = 'asset_create.html'
+    template_name = 'asset/asset_create.html'
     success_url = reverse_lazy('assets_list')
 
 
 class EditAssetView(views.UpdateView):
     model = Asset
     form_class = EditAssetForm
-    template_name = 'asset_edit.html'
+    template_name = 'asset/asset_edit.html'
     success_url = reverse_lazy('assets_list')
 
 
 class DeleteAssetView(views.DeleteView):
     model = Asset
-    template_name = 'asset_delete.html'
+    template_name = 'asset/asset_delete.html'
     success_url = reverse_lazy('assets_list')
 
 
 class MaterialsListView(views.ListView):
     model = Material
-    template_name = 'materials.html'
+    template_name = 'material/materials.html'
     ordering = ('name', 'category')
 
 
 class CreateMaterialView(views.CreateView):
     model = Material
     form_class = CreateMaterialForm
-    template_name = 'material_create.html'
+    template_name = 'material/material_create.html'
     success_url = reverse_lazy('materials_list')
 
 
 class EditMaterialView(views.UpdateView):
     model = Material
     form_class = EditMaterialForm
-    template_name = 'material_edit.html'
+    template_name = 'material/material_edit.html'
     success_url = reverse_lazy('materials_list')
 
 
 class DeleteMaterialView(views.DeleteView):
     model = Material
-    template_name = 'material_delete.html'
+    template_name = 'material/material_delete.html'
     success_url = reverse_lazy('materials_list')
