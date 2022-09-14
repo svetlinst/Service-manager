4b6be2507bba16f4f4131b71b121d5351f66307b
svetlinst <svetlinst@gmail.com>
2022-09-15 01:07:25 +0300

    Add styling changes

--- service_manager/settings.py
+++ service_manager/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
-]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'staticfiles',
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = '/media/'
