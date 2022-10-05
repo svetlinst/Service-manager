1af03afae69312d9b6c1f15dce76eb7351ad34d8
svetlinst <svetlinst@gmail.com>
2022-10-05 17:46:12 +0300

    Make the date filtering under finished orders functional

--- service_manager/reports/views.py
+++ service_manager/reports/views.py
@@ -1,3 +1,6 @@
+import datetime as dt
+
+from django.db.models import Q
 from django.shortcuts import render
 import django.views.generic as views
 
@@ -10,4 +13,14 @@ class FinishedOrdersListView(views.ListView):
     RELATED_ENTITIES = ['serviceordernote_set', 'serviceorderdetail_set', 'customer__customerasset_set', ]
 
     def get_queryset(self):
-        return ServiceOrderHeader.objects.filter(is_completed=True).prefetch_related(*self.RELATED_ENTITIES)
+        date_from = self.request.GET.get('from', None)
+        date_to = self.request.GET.get('to', None)
+
+        query_set = ServiceOrderHeader.objects.filter(is_completed=True).prefetch_related(*self.RELATED_ENTITIES)
+        if date_from and date_to:
+            date_from = dt.datetime.strptime(date_from, '%Y-%m-%d')
+            date_to = dt.datetime.strptime(date_to, '%Y-%m-%d')
+            date_to += dt.timedelta(days=1)
+            # return query_set.filter(Q(completed_on__gte=date_from))
+            return query_set.filter(Q(completed_on__gte=date_from), Q(completed_on__lte=date_to))
+        return query_set
