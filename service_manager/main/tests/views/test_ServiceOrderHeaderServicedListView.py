bcae7b7965b5401502ebcb56166159bcf9cc6347
svetlinst <svetlinst@gmail.com>
2022-11-06 09:05:23 +0200

    Add tests for ServiceOrderHeaderServicedListView

---
+++ service_manager/main/tests/views/test_ServiceOrderHeaderServicedListView.py
@@ -0,0 +1,118 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+from django.test import TestCase
+from django.urls import reverse
+
+from service_manager.customers.models import Customer, CustomerAsset
+from service_manager.main.models import ServiceOrderHeader
+from service_manager.master_data.models import CustomerType, AssetCategory, Brand, Asset
+
+UserModel = get_user_model()
+
+
+class ServiceOrderHeaderServicedListViewTests(TestCase):
+    USER_DATA = {
+        'email': 'dev@dev.com',
+        'password': 'dev',
+    }
+
+    def setUp(self):
+        UserModel.objects.create_user(**self.USER_DATA)
+
+        customer_type = CustomerType.objects.create(
+            name='Business'
+        )
+
+        customer = Customer.objects.create(
+            type=customer_type,
+            name='Testing Inc.',
+            vat='123444121',
+            email_address='testing@inc.com',
+            phone_number='100921122',
+        )
+
+        category = AssetCategory.objects.create(
+            name='Monilr',
+        )
+
+        brand = Brand.objects.create(
+            name='Apple',
+        )
+
+        asset = Asset.objects.create(
+            category=category,
+            brand=brand,
+            model_name='iPhone',
+            model_number='13 Pro',
+        )
+
+        asset2 = Asset.objects.create(
+            category=category,
+            brand=brand,
+            model_name='MacBook',
+            model_number='14 Inch Pro',
+        )
+
+        customer_asset1 = CustomerAsset.objects.create(
+            customer=customer,
+            asset=asset,
+            serial_number='SN-Apple-21',
+            product_number='California-PN_33',
+        )
+
+        customer_asset2 = CustomerAsset.objects.create(
+            customer=customer,
+            asset=asset2,
+            serial_number='MB-14-22',
+            product_number='MBook PR-44',
+        )
+
+        ServiceOrderHeader.objects.create(
+            customer=customer,
+            customer_asset=customer_asset1,
+            problem_description='First SOH Description',
+        )
+
+        ServiceOrderHeader.objects.create(
+            customer=customer,
+            customer_asset=customer_asset2,
+            problem_description='Second SOH Description',
+        )
+
+    def test_get__when_user_is_logged__expect_correct_template(self):
+        self.client.login(**self.USER_DATA)
+
+        response = self.client.get(reverse('service_orders_list_serviced'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'service_order_header/list_views/service_orders_complete.html')
+
+    def test_get__when_user_is_not_logged__expect_to_redirect(self):
+        response = self.client.get(reverse('service_orders_list_serviced'))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/login/?next=/service_orders/serviced/')
+
+    def test_get__when_all_are_pending__expect_correct_queryset(self):
+        self.client.login(**self.USER_DATA)
+
+        response = self.client.get(reverse('service_orders_list_serviced'))
+        context = response.context['object_list']
+        service_orders = ServiceOrderHeader.objects.filter(Q(is_serviced=True) & Q(is_completed=False))
+
+        self.assertEqual(len(context), len(service_orders))
+        self.assertQuerysetEqual(context, service_orders, transform=lambda x: x)
+
+    def test_get__when_one_is_serviced__expect_correct_queryset(self):
+        self.client.login(**self.USER_DATA)
+
+        service_order = ServiceOrderHeader.objects.first()
+        service_order.is_serviced = True
+        service_order.save()
+
+        response = self.client.get(reverse('service_orders_list_serviced'))
+        context = response.context['object_list']
+        service_orders = ServiceOrderHeader.objects.filter(Q(is_serviced=True) & Q(is_completed=False))
+
+        self.assertEqual(len(context), 1)
+        self.assertQuerysetEqual(context, service_orders, transform=lambda x: x)
