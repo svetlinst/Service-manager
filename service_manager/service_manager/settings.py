aa849d8bdba0316c627804f2c5599de90c9059e0
svetlinst <svetlinst@gmail.com>
2022-11-23 11:13:31 +0200

    Config changes

--- service_manager/service_manager/settings.py
+++ service_manager/service_manager/settings.py
@@ -31,6 +31,8 @@ DEBUG = bool(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
+CSRF_TRUSTED_ORIGINS = [f'https://{x}' for x in ALLOWED_HOSTS]
+
 # Application definition
 DJANGO_APPS = (
     "django.contrib.admin",
