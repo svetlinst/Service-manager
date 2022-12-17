2924bd3321c45d1e70cfcaf9ca54c85368d7d822
svetlinst <svetlinst@gmail.com>
2022-12-17 22:47:43 +0200

    Whenever a user is assigned with is_staff access, grant permissions according to the Front office, Back office and Master data editor roles

--- service_manager/service_manager/accounts/admin.py
+++ service_manager/service_manager/accounts/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 from service_manager.accounts.models import AppUser, Profile
 
+User = get_user_model()
 
-@admin.register(AppUser)
+
+@admin.register(User)
 class AppUserAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        """
+        Grant accesses to all users with is_staff permissions to the below groups.
+        Don't allow them to change any other settings.
+        """
+        super().save_model(request, obj, form, change)
+
+        groups = ['FrontOffice', 'BackOffice', 'MasterDataEditor', ]
+        if obj.is_staff:
+            form.cleaned_data['groups'] = Group.objects.filter(name__in=groups)
 
 
 @admin.register(Profile)
