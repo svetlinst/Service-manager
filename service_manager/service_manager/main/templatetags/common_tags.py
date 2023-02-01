b9796bab12717d932d6c76c343338eebf7e20d26
svetlinst <svetlinst@gmail.com>
2023-02-01 21:32:04 +0200

    Hide Service order notes in SOH detail when there are no notes and the SOH is deleted

--- service_manager/service_manager/main/templatetags/common_tags.py
+++ service_manager/service_manager/main/templatetags/common_tags.py
@@ -5,10 +5,12 @@ from service_manager.main.models import ServiceOrderHeader
 register = template.Library()
 
 
-@register.filter(name='soh_is_completed_without_notes')
-def soh_is_completed_without_notes(service_order_id):
+@register.filter(name='soh_is_completed_or_deleted_without_notes')
+def soh_is_completed_or_deleted_without_notes(service_order_id):
     service_order = ServiceOrderHeader.objects.get(pk=service_order_id)
 
     if service_order.is_completed and not service_order.serviceordernote_set.all():
+        return True
+    if not service_order.active and not service_order.serviceordernote_set.all():
         return True
     return False
