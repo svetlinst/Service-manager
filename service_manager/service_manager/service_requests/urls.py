33747f232c1bab966a2c5658ffa3830a9dcbb86f
svetlinst <svetlinst@gmail.com>
2023-11-02 09:57:52 +0200

    Add translation for service request items

--- service_manager/service_manager/service_requests/urls.py
+++ service_manager/service_manager/service_requests/urls.py
@@ -5,21 +5,21 @@ from service_manager.service_requests.views import ServiceRequestsListView, Serv
     DeleteServiceRequestView, finalize_service_request, reject_service_request, ServiceRequestCreateServiceOrder
 
 urlpatterns = [
-    path('/', ServiceRequestsListView.as_view(), name='service_requests'),
-    path('/detail/<int:pk>/', ServiceRequestDetailView.as_view(),
+    path('', ServiceRequestsListView.as_view(), name='service_requests'),
+    path('detail/<int:pk>/', ServiceRequestDetailView.as_view(),
          name='service_request_detail'),
-    path('/create/', CreateServiceRequestView.as_view(), name='create_service_request'),
-    path('/<int:pk>/', EditServiceRequestView.as_view(), name='edit_service_request'),
-    path('/<int:pk>/assign_handler/', ServiceRequestAssignHandlerView.as_view(),
+    path('create/', CreateServiceRequestView.as_view(), name='create_service_request'),
+    path('<int:pk>/', EditServiceRequestView.as_view(), name='edit_service_request'),
+    path('<int:pk>/assign_handler/', ServiceRequestAssignHandlerView.as_view(),
          name='service_request_assign_handler'),
-    path('/<int:pk>/update_resolution/', ServiceRequestUpdateResolutionView.as_view(),
+    path('<int:pk>/update_resolution/', ServiceRequestUpdateResolutionView.as_view(),
          name='service_request_update_resolution'),
-    path('/delete/<int:pk>/', DeleteServiceRequestView.as_view(),
+    path('delete/<int:pk>/', DeleteServiceRequestView.as_view(),
          name='delete_service_request'),
-    path('/<int:pk>/finalize/', finalize_service_request,
+    path('<int:pk>/finalize/', finalize_service_request,
          name='finalize_service_request'),
-    path('/<int:pk>/reject/', reject_service_request, name='reject_service_request'),
-    path('/<int:pk>/create_service_order/<int:customer_id>/',
+    path('<int:pk>/reject/', reject_service_request, name='reject_service_request'),
+    path('<int:pk>/create_service_order/<int:customer_id>/',
          ServiceRequestCreateServiceOrder.as_view(),
          name='service_request_create_order')
 ]
