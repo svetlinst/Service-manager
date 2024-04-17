03504368f641df3407282ad97d170cadaf9ecf36
svetlinst <svetlinst@gmail.com>
2024-04-17 09:19:53 +0300

    UI changes to Finalized orders under Reports

--- service_manager/service_manager/reports/views.py
+++ service_manager/service_manager/reports/views.py
@@ -1,9 +1,7 @@
 import datetime as dt
-
 from django.db.models import Q
 from django.shortcuts import render
 import django.views.generic as views
-
 from service_manager.main.models import ServiceOrderHeader
 
 
@@ -11,6 +9,20 @@ class FinishedOrdersListView(views.ListView):
     model = ServiceOrderHeader
     template_name = 'finished_orders.html'
     RELATED_ENTITIES = ['serviceordernote_set', 'serviceorderdetail_set', 'customer__customerasset_set', ]
+    context_object_name = 'service_orders'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        request = self.request.GET.copy()
+        params = request.pop('page', True) and request.urlencode()
+        context['params'] = params
+        context['orders_count'] = self.get_queryset().count()
+        context['from_date'] = self.request.GET.get('from', '')
+        context['to_date'] = self.request.GET.get('to', '')
+
+        return context
 
     def get_queryset(self):
         date_from = self.request.GET.get('from', None)
@@ -21,6 +33,5 @@ class FinishedOrdersListView(views.ListView):
             date_from = dt.datetime.strptime(date_from, '%Y-%m-%d')
             date_to = dt.datetime.strptime(date_to, '%Y-%m-%d')
             date_to += dt.timedelta(days=1)
-            # return query_set.filter(Q(completed_on__gte=date_from))
             return query_set.filter(Q(completed_on__gte=date_from), Q(completed_on__lte=date_to))
         return query_set
